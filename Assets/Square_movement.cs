bfc461b796e525236e60eee191eb860bc0384d2c
David Nguyen <davidnguyen@Davids-MacBook-Pro-3.local>
2022-09-21 21:33:32 -0400

    Testing with Unity

---
+++ Assets/Square_movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square_movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Debug");
+        print("Debug");
+        print("Debug");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
