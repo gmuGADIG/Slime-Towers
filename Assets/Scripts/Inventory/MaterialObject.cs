f5672d4fdf92607311d1d5b306187978a2cdf721
Nicholas Szymonik <nicholascoles.a@gmail.com>
2022-09-28 20:09:53 -0400

    made object disappear on collection. created debug logger for inventory.

--- Assets/Scripts/Inventory/MaterialObject.cs
+++ Assets/Scripts/Inventory/MaterialObject.cs
@@ -24,13 +24,19 @@ public class MaterialObject : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            collision.gameObject.GetComponent<Inventory>().AddItem(material, 1); //TODO multi-item pickups?
+            Inventory inventory = collision.gameObject.GetComponent<Inventory>();
+            inventory.AddItem(material, 1); //TODO multi-item pickups?
+            inventory.logStatus();
+
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
             collectEffect.Play();
             Destroy(gameObject,5);
+            
         }
     }
 }
