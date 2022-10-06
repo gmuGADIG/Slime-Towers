4fad789934d6aa3237acb368b4016ba426ac90ec
Stephen H <shapp@masonlive.gmu.edu>
2022-10-05 21:54:08 -0400

    Continued work on manager

---
+++ Assets/Scripts/ManagerTestButtons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerTestButtons : MonoBehaviour
+{
+    private GameObject gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Manager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void attackPressed() {
+        
+    }
+}
