78cf954f30f2044546106971586174533ea00d1b
Christopher Chay <2vendor@hisfootsteps.net>
2022-10-05 21:05:59 -0400

    Chay: Added Instruction Menu. Can traverse to Title Menu and back

---
+++ Assets/Return.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Return : MonoBehaviour
+{
+    public void HandleButton(){
+        SceneManager.LoadScene("Title Menu");
+        //Debug.Log( "Button Clicked" );
+        //SceneManager.UnloadScene( "Instruction Menu" );
+    }
+}
