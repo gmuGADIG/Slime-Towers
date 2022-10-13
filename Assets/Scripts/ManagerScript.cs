2aa13af7852ee8ff36d47cea25ab462e16cc35ca
Stephen H <shapp@masonlive.gmu.edu>
2022-10-12 21:03:45 -0400

    Conintued work on manager script

--- Assets/Scripts/ManagerScript.cs
+++ Assets/Scripts/ManagerScript.cs
@@ -2,33 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GameState {
-    EXPLORE, //Player explores cave, build towers
-    ATTACK, //Enemies spawn and attack drill
-    DIALOGUE, //Player/enemy movement restricted, but not complete pause.
-                //Animations and sounds still play
-    PAUSE //Complete pause, animations and sounds stop
-}
-
 public class ManagerScript : MonoBehaviour
 {
     
     private GameState gameState;
     private AudioListener audioManager;
-    public static ManagerScript gm;
-
-    private void Awake() {
-        if(gm == null){
-            gm = this;
-        }else{
-            Destroy(gameObject);
-        }
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
         setGameState(GameState.EXPLORE);
-        audioManager = gameObject.GetComponent<AudioListener>();
+        audioManager = FindObjectOfType<AudioListener>();
     }
 
     // Update is called once per frame
@@ -49,19 +33,19 @@ public class ManagerScript : MonoBehaviour
         switch (this.gameState) {
             case GameState.EXPLORE:
                 Time.timeScale = 1f;
-                AudioListener.volume = 0.5f;
+                AudioListener.volume = 0.7f;
                 break;
             case GameState.ATTACK:
                 Time.timeScale = 1f;
-                AudioListener.volume = 0.5f;
+                AudioListener.volume = 1f;
                 break;
             case GameState.DIALOGUE:
                 Time.timeScale = 1f;
-                AudioListener.volume = 0.5f;
+                AudioListener.volume = 0.4f;
                 break;
             case GameState.PAUSE:
                 Time.timeScale = 0f;
-                AudioListener.volume = 0.5f;
+                AudioListener.volume = 0.1f;
                 break;
             default:
                 break;
