3a5db5fe966ac25a6b5e52bbe827628ed749a07c
Hannah Jackowski <hannah.r.jackowski@gmail.com>
2022-10-24 15:23:13 -0400

    Moved player movement into a folder for player scripts. Made a test UI scene with ResourceNode interaction.

---
+++ Assets/Scripts/Player Scripts/PopupManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupManager : MonoBehaviour
+{
+    public Text popupText;
+    public Image popupPanel;
+    private IInteractable objectShowing;
+
+    void Start()
+    {
+        // Hide my elements
+        hide_popup();
+    }
+
+    /*
+     *  If a popup is showing, check if 'E' is pressed to interact.
+     */
+    void Update() {
+        if ( objectShowing != null ) {
+            if ( Input.GetKeyDown(KeyCode.E) ) {
+                objectShowing.interact();
+                objectShowing = null;
+                hide_popup();
+            }
+        }
+    }
+
+    /*
+     *  Given an interactable object, display the UI and text.
+     *  Will implement popups with sprites (ex: NPC profiles) as an overloaded function.
+     */
+    void show_popup( IInteractable obj )
+    {
+        objectShowing = obj;
+        popupText.text = obj.popup_text();
+
+        popupPanel.enabled = true;
+    }
+
+    /*
+     *  Hide the elements of the Popup UI.
+     */
+    void hide_popup()
+    {
+        popupText.text = " ";
+        popupPanel.enabled = false;
+    }
+
+    /*
+     *  Checks if player runs close enough to an interactable to display the popup.
+     *  Might want to change to being based on which is closer, rather than who it "bumps into" most recently.
+     */
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if ( collision.tag.Equals( "Interactable" ) ) {
+            objectShowing = collision.GetComponent<IInteractable>();
+            show_popup( objectShowing );
+        }
+    }
+
+    /*
+     *  If running out of range of an interactable, check if it's the currently displayed popup. If it is, hide the popup.
+     */
+    private void OnTriggerExit2D(Collider2D collision) {
+        if ( collision.tag.Equals( "Interactable" ) ) {
+            if ( collision.GetComponent<IInteractable>().Equals( objectShowing) ) {
+                hide_popup();
+                objectShowing = null;
+            }
+        }
+    }
+
+}
