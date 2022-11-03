1a85d755400edf95bbe821341d8d50dc9aa5d041
Tushar Rangaswamy <trangasw@gmu.edu>
2022-11-02 21:41:31 -0400

    Basic waves are setup

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -1,22 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Wave : MonoBehaviour
 {
-
-    public List<GameObject> enemies;
-    public delegate void WaveComplete();
-    public bool isActive { get { return enemies.Count != 0; } }
+    public HashSet<GameObject> enemies;
+    public UnityEvent waveComplete;
+    public float totalWaveTime;
+    public float remainingTimeInWave;
+    public bool active;
+    
+    public List<EnemySpawner> spawners;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemies = new HashSet<GameObject>();
+        foreach (EnemySpawner i in spawners)
+        {
+            Debug.Log(i.name);
+            i.onSpawn.AddListener(AddEnemyToWave);
+        }
+        remainingTimeInWave = totalWaveTime;
+
+        setActive(true);
+    }
+    
+    private void Update()
+    {
+        if(remainingTimeInWave > 0)
+        {
+            remainingTimeInWave -= Time.deltaTime;
+        } else
+        {
+            setActive(false);
+
+        }
+
+    }
+    public void AddEnemyToWave(GameObject enemy)
+    {
+
+        enemy.GetComponent<EnemyBehavior>().onDeath.AddListener(RemoveEnemy);
+        this.enemies.Add(enemy);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void RemoveEnemy(GameObject enemy)
     {
-        
+        enemies.Remove(enemy);
+        if (enemies.Count == 0)
+        {
+            waveComplete.Invoke();
+            Debug.Log("Wave Complete");
+        }
+    }
+
+    void setActive(bool value)
+    {
+        active = value;
+        foreach(EnemySpawner i in spawners)
+        {
+            i.active = value;
+        }
     }
 }
