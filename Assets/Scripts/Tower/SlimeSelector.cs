31d148b759799e76c762c65655b9aaec0e4d1c8a
Aiden Young <ayoung49@gmu.edu>
2022-11-09 21:58:29 -0500

    Made some advancements in the manager that and inventory making if the slimes don't exist in inventory then you can't select the upgrade

--- Assets/Scripts/Tower/SlimeSelector.cs
+++ Assets/Scripts/Tower/SlimeSelector.cs
@@ -1,13 +1,97 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class SlimeSelector : MonoBehaviour
 {
 	//The slime type of this selector
 	public GameObject towerParent;
+    public GameObject defaultButton;
+    public GameObject zapButton;
+    public GameObject fireButton;
+    public GameObject iceButton;
+    private UnityAction<string, bool> buttonAction;
 
-	public void setSlime(string slime)
+    void Awake()
+    {
+        int invAmount;
+        bool hasMat = false;
+        hasMat = Inventory.materials.TryGetValue(SlimeTowers.MaterialType.SLIME, out invAmount);
+        if (!hasMat || invAmount < 1)
+        {
+            defaultButton.GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            defaultButton.GetComponent<Button>().interactable = true;
+        }
+        hasMat = Inventory.materials.TryGetValue(SlimeTowers.MaterialType.ZAP_SLIME, out invAmount);
+        if (!hasMat || invAmount < 1)
+        {
+            zapButton.GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            zapButton.GetComponent<Button>().interactable = true;
+        }
+        hasMat = Inventory.materials.TryGetValue(SlimeTowers.MaterialType.FIRE_SLIME, out invAmount);
+        if (!hasMat || invAmount < 1)
+        {
+            fireButton.GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            fireButton.GetComponent<Button>().interactable = true;
+        }
+        hasMat = Inventory.materials.TryGetValue(SlimeTowers.MaterialType.ICE_SLIME, out invAmount);
+        if (!hasMat || invAmount < 1)
+        {
+            iceButton.GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            iceButton.GetComponent<Button>().interactable = true;
+        }
+
+    }
+
+    public void setSlime(string slime, bool canClick)
 	{
-		towerParent.GetComponent<Tower>().setSlime(slime);
-		Destroy(gameObject);
+        if (canClick)
+        {
+            Slime_Type slimeType = Slime_Type.None;
+            int invAmount;
+            bool hasMat = false;
+            if (slimeType.Equals("Default"))
+            {
+                slimeType = Slime_Type.Default;
+                hasMat = Inventory.materials.TryGetValue(SlimeTowers.MaterialType.SLIME, out invAmount);
+            }
+            else if (slimeType.Equals("Zap"))
+            {
+                slimeType = Slime_Type.Zap;
+                hasMat = Inventory.materials.TryGetValue(SlimeTowers.MaterialType.ZAP_SLIME, out invAmount);
+            }
+            else if (slimeType.Equals("Fire"))
+            {
+                slimeType = Slime_Type.Fire;
+                hasMat = Inventory.materials.TryGetValue(SlimeTowers.MaterialType.FIRE_SLIME, out invAmount);
+            }
+            else if (slimeType.Equals("Ice"))
+            {
+                slimeType = Slime_Type.Ice;
+                hasMat = Inventory.materials.TryGetValue(SlimeTowers.MaterialType.ICE_SLIME, out invAmount);
+            }
+            else if (slimeType.Equals("Remove"))
+            {
+                slimeType = Slime_Type.None;
+            }
+            towerParent.GetComponent<Tower>().setSlime(slimeType);
+            Destroy(gameObject);
+        }
+        else
+        {
+
+        }
 	}
 }
