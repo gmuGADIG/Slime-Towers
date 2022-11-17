89c7522fe96f66baaa2f9c25a270207337eed60c
Jack Whritenour <jwhriten@gmu.edu>
2022-11-16 22:04:13 -0500

    worked on making laser worky

--- Assets/Prefab/laserextenderscript.cs
+++ Assets/Prefab/laserextenderscript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class laserscript : MonoBehaviour
+public class laserextenderscript : MonoBehaviour
 {
 
     public GameObject laserObject;
@@ -16,22 +16,14 @@ public class laserscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        generateLaser();
+        
     }
 
     void generateLaser()
     {
         RaycastHit2D Laser = Physics2D.Raycast(transform.position, Vector2.right);
-        Debug.Log(Laser.distance);
-
-        if (Laser.collider != null)
-                {
-
-            laserextenderscript other = Laser.collider.GetComponent<laserextenderscript>();
-            
-            
-                
-                }
+        Debug.Log(Laser);
+        
 
         // if chekc hits a crystal then fire another raycast
     }
